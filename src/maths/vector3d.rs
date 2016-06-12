ee09959a3741726a33f2369bad3fc80f17f66a6c
Erlend Røsjø <erlend@erlend.net>
2016-06-12 13:51:05 +0200

    Refactored tests

--- src/maths/vector3d.rs
+++ src/maths/vector3d.rs
@@ -92,3 +92,86 @@ impl std::ops::Neg for Vector3D {
         return Vector3D {x: -self.x, y: -self.y, z: -self.z };
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Vector3D;
+
+    #[test]
+    fn magnitude_valid_input_should_return_correct_value() {
+        let v = Vector3D { x: -3.0, y: 2.0, z: 5.0 };
+        let expected_magnitude = (38 as f64).sqrt();
+        assert_eq!(v.magnitude(), expected_magnitude);
+    }
+
+    #[test]
+    fn normalize_valid_input_should_return_correct_value() {
+        let v = Vector3D { x: 2.0, y: -5.0, z: 4.0 };
+
+        let expected_x = 2.0 / (3.0 * (5 as f64).sqrt());
+        let expected_y = -(5 as f64).sqrt() / 3.0;
+        let expected_z = 4.0 / (3.0 *(5 as f64).sqrt());
+
+        let result = v.normalize();
+        assert_eq!(result.x, expected_x);
+        assert_eq!(result.y, expected_y);
+        assert_eq!(result.z, expected_z);
+    }
+
+    #[test]
+    fn negative_valid_input_should_return_correct_value() {
+        let v = Vector3D { x: 2.0, y: 4.0, z: -5.0 };
+
+        let result = -v;
+        assert_eq!(-2.0, result.x);
+        assert_eq!(-4.0, result.y);
+        assert_eq!(5.0, result.z);
+    }
+
+    #[test]
+    fn dot_product_valid_input_should_return_correct_value() {
+        let v1 = Vector3D { x: 1.0, y: 2.0, z: 3.0 };
+        let v2 = Vector3D { x: 3.0, y: 4.0, z: 5.0 };
+
+        let expected_result = 26.0;
+
+        let result = v1 * v2;
+
+        assert_eq!(expected_result, result);
+    }
+
+    #[test]
+    fn cross_product_valid_input_should_return_correct_value() {
+        let v1 = Vector3D { x: 1.0, y: 2.0, z: 3.0 };
+        let v2 = Vector3D { x: 3.0, y: 4.0, z: 5.0 };
+
+        let result = v1.cross_product(&v2);
+
+        assert_eq!(-2.0, result.x);
+        assert_eq!(4.0, result.y);
+        assert_eq!(-2.0, result.z);
+    }
+
+    #[test]
+    fn add_valid_input_should_return_correct_result() {
+        let v1 = Vector3D { x: 1.0, y: 2.0, z: 3.0 };
+        let v2 = Vector3D { x: 3.0, y: 4.0, z: 5.0 };
+
+        let result = v1 + v2;
+
+        assert_eq!(4.0, result.x);
+        assert_eq!(6.0, result.y);
+        assert_eq!(8.0, result.z);
+    }
+
+    #[test]
+    fn mult_valid_input_should_return_correct_result() {
+        let v = Vector3D { x: 1.0, y: 2.0, z: 3.0};
+
+        let result = v * 3.0;
+
+        assert_eq!(3.0, result.x);
+        assert_eq!(6.0, result.y);
+        assert_eq!(9.0, result.z);
+    }
+}
