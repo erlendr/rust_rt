07e255450cbd91f28fb7fd00dae7964f39a0f1bb
Erlend Røsjø <erlend@erlend.net>
2016-06-12 14:19:38 +0200

    Added Color multiply and tests

--- src/maths/color.rs
+++ src/maths/color.rs
@@ -20,6 +20,15 @@ impl Color {
             special: (self.special + c2.special),
         };
     }
+
+    pub fn multiply(&self, c2: &Color) -> Color {
+        return Color {
+            red: (self.red * c2.red),
+            green: (self.green * c2.green),
+            blue: (self.blue * c2.blue),
+            special: (self.special * c2.special),
+        };
+    }
 }
 
 impl std::ops::Add for Color {
@@ -32,6 +41,20 @@ impl std::ops::Add for Color {
             green: (self.green + other.green),
             blue: (self.blue + other.blue),
             special: (self.special + other.special),
+        };
+    }
+}
+
+impl std::ops::Mul<Color> for Color {
+    type Output = Color;
+
+    /// Multiplies two colors
+    fn mul(self, other: Color) -> Color {
+        return Color {
+            red: (self.red * other.red),
+            green: (self.green * other.green),
+            blue: (self.blue * other.blue),
+            special: (self.special * other.special),
         };
     }
 }
@@ -49,6 +72,45 @@ mod tests {
 
         assert_eq!(1.0, result.red);
         assert_eq!(1.0, result.green);
+        assert_eq!(0.0, result.blue);
+        assert_eq!(0.0, result.special);
+    }
+
+    #[test]
+    fn add_fn_valid_input_should_return_correct_value() {
+        let red_color = Color { red: 1.0, green: 0.0, blue: 0.0, special: 0.0 };
+        let green_color = Color { red: 0.0, green: 1.0, blue: 0.0, special: 0.0 };
+
+        let result = red_color.add(&green_color);
+
+        assert_eq!(1.0, result.red);
+        assert_eq!(1.0, result.green);
+        assert_eq!(0.0, result.blue);
+        assert_eq!(0.0, result.special);
+    }
+
+    #[test]
+    fn multiply_valid_input_should_return_correct_value() {
+        let color_1 = Color { red: 0.5, green: 0.0, blue: 0.0, special: 0.0 };
+        let color_2 = Color { red: 0.3, green: 0.0, blue: 0.0, special: 0.0 };
+
+        let result = color_1 * color_2;
+
+        assert_eq!(0.15, result.red);
+        assert_eq!(0.0, result.green);
+        assert_eq!(0.0, result.blue);
+        assert_eq!(0.0, result.special);
+    }
+
+    #[test]
+    fn multiply_fn_valid_input_should_return_correct_value() {
+        let color_1 = Color { red: 0.5, green: 0.0, blue: 0.0, special: 0.0 };
+        let color_2 = Color { red: 0.3, green: 0.0, blue: 0.0, special: 0.0 };
+
+        let result = color_1.multiply(&color_2);
+
+        assert_eq!(0.15, result.red);
+        assert_eq!(0.0, result.green);
         assert_eq!(0.0, result.blue);
         assert_eq!(0.0, result.special);
     }
